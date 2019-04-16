85e1fb728219c89363391301445f820609f4fa32
sho kisaragi <sho@oshiire.to>
2019-04-16 13:17:46 +0900

    fix typo

--- siege.sh
+++ siege.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export SIEGERC=/app/local/siege.conf
-/app/local/bin/siege -q -c200 -r500 -t10S --header='Authorization: Basic c3RvcmVmcm9udDpwcmV2aWV3MTIz=\n' https://production-ap01-cainz.demandware.net/s/SiteGenesis/mens-straight-fit-shorts/54736828.html?lang=en_US
+/app/local/bin/siege -c200 -r500 -t10S --header='Authorization: Basic c3RvcmVmcm9udDpwcmV2aWV3MTIz=\n' https://production-ap01-cainz.demandware.net/s/SiteGenesis/mens-straight-fit-shorts/54736828.html?lang=en_US
